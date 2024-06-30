33060a78f385c9a1911febdbe54e77debafc11f1
Shivam Patel <shivam.patel94@rutgers.edu>
2024-06-29 23:51:45 -0400

    gh training initial outline'

---
+++ introductions/shivampatel.py
@@ -0,0 +1,4 @@
+def intro():
+    print('Hi! My name is Shivam Patel. I\'m a senior Computer Science and Economics major.' + 
+          '\nI\'ve been a part of AIAA and RPL since I was a freshman. It\'s my third and final year serving you all as a team lead. ' + 
+          '\nI\'m always open to talk and I really want to make sure everyone feels welcome to be their best on our team.\n')
---
+++ main.py
@@ -0,0 +1,17 @@
+import sys # Please don't touch this
+sys.path.append('./introductions') # or this (bonus points if you know why)
+
+# Put all imports below this line
+from introductions import shivampatel # import the file containing your individual introduction blurb
+
+# Put all imports above this line
+
+def main():
+    # Call your intro function below this line
+    shivampatel.intro() 
+
+    # Call you intro function above this line
+
+
+if __name__ == '__main__':
+    main()
