b70d334350bf512b61453fcc3628fb57e8179cba
Kashvi Chandwani <rutgersaiaa@gmail.com>
2024-06-30 00:37:49 -0400

    Github Training: Kashvi Chandwani

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ sys.path.append('./introductions') # or this (bonus points if you know why)
 
 # Put all imports below this line
 from introductions import shivampatel # import the file containing your individual introduction blurb
-
+from introductions import kashvichandwani
 # Put all imports above this line
 
 def main():
     # Call your intro function below this line
     shivampatel.intro() 
+    kashvichandwani.intro()
 
     # Call you intro function above this line
 
