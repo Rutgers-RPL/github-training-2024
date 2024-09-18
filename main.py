0d6d5d8b5290e33de6fe84336b8005dcf63b0fca
wispl <ivanz0@protonmail.com>
2024-09-18 18:54:26 +0000

    Add Ivan introduction

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ sys.path.append('./introductions') # or this (bonus points if you know why)
 # Put all imports below this line
 from introductions import shivampatel # import the file containing your individual introduction blurb
 from introductions import kashvichandwani
+from introductions import ivanz
 # Put all imports above this line
 
 def main():
     # Call your intro function below this line
     shivampatel.intro() 
     kashvichandwani.intro()
+    ivanz.intro()
 
     # Call you intro function above this line
 
