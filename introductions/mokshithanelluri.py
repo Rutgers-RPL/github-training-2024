1b7791b9cf5e77841fe957477fce28fa6d54e11a
Mokshitha Nelluri <mokshi.nelluri@gmail.com>
2024-09-20 19:47:04 -0400

    GitHub Training: *Mokshitha* *Nelluri*

---
+++ introductions/mokshithanelluri.py
@@ -0,0 +1,4 @@
+def intro():
+    print("Hi, I'm Mokshitha, a sophomore CS major recently transferred from India."+
+          "\n At my previous university, I worked on research AI for a student project called Mars Rover Manipal."+
+          "\n I’m really excited to explore new topics and contribute to the team at RRPL!\n")
